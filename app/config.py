e9887d51d451b952e0c1892be4bebeae6935bdff
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-03 19:19:56 +0300

    fix bugs on DB

--- app/config.py
+++ app/config.py
@@ -1,23 +1,42 @@
 import os
 
+
 class Config:
-    SECRET_KEY = '06b428d12ac878e0e11f4a432921c41a'  
+    """Main configurations class"""
+
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://alvin:alvin123@localhost/mboga'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+    SUBJECT_PREFIX = 'BLOG'
+    SENDER_EMAIL = 'alvinmichoma@gmail.com'
+
+ # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/mboga'
+    DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/pitch'
 
-class ProdConfig(Config):
+class TestConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/pitch'
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/mboga_test'
+
+
 config_options = {
-    'development':DevConfig,
-    'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
--- manage.py
+++ manage.py
@@ -1,15 +1,24 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from  flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server', Server)
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
-if __name__ =='__main__':
-    manager.run( ) 
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+
+if __name__ == '__main__':
+    manager.run()
