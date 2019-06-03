ccb5b0e349548e4306aa41526652fb2eef43c9c9
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-03 12:31:53 +0300

    add configurations on config.py

--- config.py
+++ config.py
@@ -1,15 +1,32 @@
+import os
+
 
 class Config:
-    pass
+    """Main configurations class"""
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/blog'
+    SECRET_KEY = 'SECRET_KEY'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-class TestConfig(Config):
-    pass
+class ProdConfig(Config):
+    """Production configuration class that inherits from the main configurations class"""
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/blog'
+
 
 class DevConfig(Config):
+    """Configuration class for development stage of the app"""
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
 
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
